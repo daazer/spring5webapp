442254fa31ff31175dc9ef3d77390a4a6362e1c2
daazer <m.r.alipour.ir@gmail.com>
2020-11-04 13:07:44 +0330

    Added Publisher

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -1,44 +1,60 @@
 package guru.springframework.spring5webapp.bootstrap;
 
-import guru.springframework.spring5webapp.domain.Author;
-import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.model.Author;
+import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
-import org.springframework.boot.CommandLineRunner;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
-
+/**
+ * Created by jt on 5/16/17.
+ */
 @Component
-public class BootStrapData implements CommandLineRunner {
+class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
-    private final AuthorRepository authorRepository;
-    private final BookRepository bookRepository;
+    private AuthorRepository authorRepository;
+    private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
-    public void run(String... args) throws Exception {
-        Author Reza = new Author("reza" , "rezaaaaa");
-        Book ddd = new Book("Domain Driven Design" , "123456");
-        Reza.getBooks().add(ddd);
-        ddd.getAuthors().add(Reza);
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        initData();
+    }
 
-        authorRepository.save(Reza);
+    private void initData(){
+
+        Publisher publisher = new Publisher();
+        publisher.setName("foo");
+        publisher.setAddress("12th Street, LA");
+        publisherRepository.save(publisher);
+
+        //Eric
+        Author eric = new Author("Eric", "Evans");
+        Book  ddd = new Book("Domain Driven Design", "1234", publisher);
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
         bookRepository.save(ddd);
 
-        Author Radin = new Author("Rod" , "Iconic");
-        Book noEJB = new Book("J2EE DEV W EJB" , "321968510");
-        Radin.getBooks().add(noEJB);
-        noEJB.getAuthors().add(Radin);
 
-        authorRepository.save(Radin);
+        //Rod
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "23444", publisher );
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(rod);
         bookRepository.save(noEJB);
-
-        System.out.println("Started in Bootstratp");
-        System.out.println("Number of Books:" + bookRepository.count());
-
     }
 }
